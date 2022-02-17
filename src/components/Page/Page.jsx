7b832c2473c75dfd8038aa00bb672e2362eb90e8
walidhassan1011 <walidhassan009@gmail.com>
2022-02-18 00:46:11 +0200

    homePageui-quiz-app

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Useforecast } from '../../context/ForecastProvider'
-
+import React from "react";
+import { Useforecast } from "../../context/ForecastProvider";
+import "./page.css";
 function Page() {
-    const {name}=Useforecast()
+  const { name } = Useforecast();
   return (
-    <div><h1>{name}</h1></div>
-  )
+    <section className="main">
+      <div className="wrap"></div>
+    </section>
+  );
 }
 
-export default Page
+export default Page;
