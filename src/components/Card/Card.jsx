701991516d921ca36971b38d1c3580b6fcaa0576
walidhassan1011 <walidhassan009@gmail.com>
2022-02-23 20:46:41 +0200

    90%-of-quizApp

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,38 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Useforecast } from "../../context/ForecastProvider";
 import "./Card.css";
 function Card() {
+  const history = useHistory();
   const { categoryName, index, setindex, answers, questions } = Useforecast();
   const [score, setscore] = useState(0);
+  const [selected, setselected] = useState();
+  const [btndisable, setbtndisable] = useState(false);
+
   function decodeString(str) {
     const textArea = document.createElement("textarea");
     textArea.innerHTML = str;
     return textArea.value;
   }
+  console.log(questions[index].correct_answer);
+  const handlecheck = (answer) => {
+    setselected(answer);
+    setbtndisable(!btndisable);
+    if (answer === questions[index].correct_answer) {
+      setscore(score + 1);
+    }
+  };
+  const handleselect = (answer) => {
+    console.log(selected);
+
+    if (selected === answer && answer !== questions[index].correct_answer) {
+      return "wrong";
+    } else if (answer === questions[index].correct_answer) {
+      return "right";
+    }
+  };
   return (
     <div className="card-frame">
-      <button
-        onClick={() => {
-          setindex(index + 1);
-        }}
-      >
-        fff
-      </button>
       <h1>question number:{index + 1}</h1>
       <div className="score-category">
         <span>score:{score}</span>
@@ -31,11 +46,38 @@ function Card() {
           {answers &&
             answers.map((answer, index) => {
               return (
-                <div className="answer-frame" key={index}>
+                <button
+                  onClick={() => {
+                    handlecheck(answer);
+                  }}
+                  className={`answer-frame  ${
+                    selected && handleselect(answer)
+                  }`}
+                  key={index}
+                  disabled={btndisable}
+                >
                   <span>{decodeString(answer)}</span>
-                </div>
+                </button>
               );
             })}
+        </div>
+        <div className="btn-frame">
+          <button
+            onClick={() => {
+              setindex(index + 1);
+              setselected();
+              setbtndisable(!btndisable);
+            }}
+          >
+            next
+          </button>
+          <button
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            quit
+          </button>
         </div>
       </div>
     </div>
